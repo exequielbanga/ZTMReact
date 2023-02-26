230c738e4bbc8d32d48476357c87c9f9b755d93e
Exequiel Banga <exe@192.168.1.10>
2023-02-26 14:22:12 -0300

    all views are function components

--- monsters-rolodex/src/Components/Card-list/card-list.component.jsx
+++ monsters-rolodex/src/Components/Card-list/card-list.component.jsx
@@ -1,24 +1,17 @@
-import { Component } from "react";
-
 import Card from "../Card/card.component";
 
 import './card-list.styles.css'
 
-class CardList extends Component{
-    render(){
-        const {monsters} = this.props;
+const CardList = ({ monsters }) =>
+(
+    <div className="card-list">
+        {monsters.map((monster) => {
+            return (
+                <Card monster={monster} key={monster.id} />
+            )
+        })}
+    </div>
+)
 
-        return (
-        <div className="card-list">
-            {monsters.map((monster) =>{
-                return(
-                    <Card monster={monster} key={monster.id}/>     
-                )
-            })}
-        </div>
-        );
-        
-    }
-}
 
 export default CardList;
