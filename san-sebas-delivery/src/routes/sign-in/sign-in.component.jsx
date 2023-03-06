6b4cebe84739b3b4855781f2d11265cfeaa916e4
exequiel <exequiel.banga@gmail.com>
2023-03-06 09:48:46 -0300

    google sign in redirect

--- san-sebas-delivery/src/routes/sign-in/sign-in.component.jsx
+++ san-sebas-delivery/src/routes/sign-in/sign-in.component.jsx
@@ -1,8 +1,6 @@
 import { 
     signInWithGooglePopup, 
-    signInWithGoogleRedirect,
     createUserDocumentFromAuth,
-    auth 
 } from "../../utils/firebase/firebase.utils"
 
 import { useEffect } from "react"
@@ -10,16 +8,6 @@ import { getRedirectResult } from "firebase/auth"
 import { async } from "@firebase/util"
 
     const SignIn = () =>{
-
-    useEffect(()=>{
-        async function fetchData(){
-            const response = await getRedirectResult(auth)
-            if (response){
-                const userdocRef = await createUserDocumentFromAuth(response.user)
-            }
-        }
-        fetchData()
-    },[])
         
     const logInGoogleUser = async () => {
         const {user} = await signInWithGooglePopup()
@@ -31,7 +19,6 @@ import { async } from "@firebase/util"
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logInGoogleUser}>Sign in with Google popup</button>
-            <button onClick={signInWithGoogleRedirect}>Sign in with Google redirect</button>
         </div>
     )
 }
